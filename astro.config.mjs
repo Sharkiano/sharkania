98ae9b31a8c805ae98625647c0eed041f91b5c07
Sharkiano <brujulamarketing.cl@gmail.com>
2026-03-07 08:38:24 -0300

    config: preparar para deploy en Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   output: 'server',
   adapter: cloudflare({
+    imageService: 'compile',
     platformProxy: {
       enabled: true,
     },
